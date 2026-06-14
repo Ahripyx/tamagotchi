c7cc839d61166726dd0a4a5ce7fe36c7f8c19a7f
Ahri <ahri.root@gmail.com>
2026-06-14 14:25:52 -0400

    Window size now set to similar size as a tamadotchi

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 360,
+    height: 480,
+    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
